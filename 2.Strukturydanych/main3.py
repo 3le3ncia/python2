de8855aa74e4ac05dafddbbe7c31bb2a46d254e9
Damian Górski <rezet9800@gmail.com>
2023-06-02 18:21:46 +0200

    Usunięcie wszystkiego bo jestem mądry inaczej

--- 2.Strukturydanych/main3.py
+++
@@ -1,4 +0,0 @@
-slowa = ["burak", "cukinia", "pomidor", "cytryna", "ananas", "papryka", "dynia"]
-litera = input("Podaj literę: ")
-pasujaceslowa = [word for word in slowa if word.startswith(litera.lower())]
-print(f"Słowa zaczynające się na literę {litera}: {pasujaceslowa}")
--- Insrukcjesterujące/main.py
+++
@@ -1,6 +0,0 @@
-#zadanie1
-wiek = int(input("Ile masz lat: "))
-if wiek >= 18:
-    print("Jesteś pełnoletni")
-else:
-    print("Jesteś niepełnoletni")
